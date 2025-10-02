9a17302efa1340afe22ce2895347396fffce6f62
subhasish20 <subhasishjena8280@gmail.com>
2025-10-02 22:17:07 +0530

    Array

---
+++ Array/1D_Array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int array[3] = {4,5,6};
+    int i;
+    for ( i = 0; i < 3; i++)
+    {
+        printf("%d\n",array[i]);
+    }
+    
+
+    return 0;
+}
