63b753fad34e8fee90040846ef55fec733fcef14
subhasish20 <subhasishjena8280@gmail.com>
2025-10-10 13:13:21 +0530

    file

---
+++ Array/subarrays_print.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#define MAX 100
+using namespace std;
+
+int i, j, k, size;
+int array[MAX];
+int DisplaySubarray(int array[], int size);
+
+int main()
+{
+    cout<<"Enter how many elements you want to store :";
+    cin>>size;
+    cout<<"Enter the elements of the array :"<<endl;
+    for(i = 0; i<size; i++)
+    {
+        cin>>array[i];
+    }
+
+    DisplaySubarray(array, size);
+
+
+    return 0;
+}
+int DisplaySubarray(int array[], int size)
+{
+
+    cout<<"the subarray of the given array are :\n";
+    for( i = 0 ; i < size; i++)
+    {
+        for(j = i; j < size; j++)
+        {
+            for(k = i; k <=j; k++)
+            {
+                cout<<array[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
