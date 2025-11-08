c003ad515b216773fa79fa61520571d7952fdd8f
subhasish20 <subhasishjena8280@gmail.com>
2025-11-08 15:55:14 +0530

    files

---
+++ Backtracking/N_Queen.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#define MAX_SIZE 4
+using namespace std;
+
+bool IsSafe()
+{
+
+}
+
+
+int main()
+{
+    int board[MAX_SIZE][MAX_SIZE];
+
+
+
+    return 0;
+
+}
